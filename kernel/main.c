4d6520c0b81fb462f9ce89e420cd1bda44cf6261
Skye-ye <saber.ap.saikou@gmail.com>
2025-11-21 10:46:43 +0800

    printf test

--- kernel/main.c
+++ kernel/main.c
@@ -2,13 +2,29 @@
 #include "defs.h"
 #include "riscv.h"
 
+void test_printf_basic() {
+  printf("Testing integer: %d\n", 42);
+  printf("Testing negative: %d\n", -123);
+  printf("Testing zero: %d\n", 0);
+  printf("Testing hex: 0x%x\n", 0xABC);
+  printf("Testing string: %s\n", "Hello");
+  printf("Testing char: %c\n", 'X');
+  printf("Testing percent: %%\n");
+}
+
+void test_printf_edge_cases() {
+  printf("INT_MAX: %d\n", 2147483647);
+  printf("INT_MIN: %ld\n", -2147483648);
+  // printf("NULL string: %s\n", (char *)0);
+  printf("Empty string: %s\n", "");
+}
+
 // start() jumps here in supervisor mode on all CPUs.
 void main() {
   consoleinit();
   printfinit();
-  printf("Hello, world!\n");
-  long long id = 2023302111369;
-  printf("My ID is %lld\n", id);
-  clear_screen();
+  test_printf_basic();
+  test_printf_edge_cases();
+  // clear_screen();
   panic("main");
 }
